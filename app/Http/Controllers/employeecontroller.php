5719b3007b164afb31af847f925b6394bec16f09
unknown <franzaaronseballos@gmail.com>
2026-05-25 13:43:24 +0800

    routes

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Fascades\DB;
+use Illuminate\Support\Facades\DB;
 use Response;
 use Illuminate\Http\Request;
 use App\Models\employee;
@@ -9,59 +9,57 @@ use App\Models\employee;
 class employeecontroller extends Controller
 {
     public function index()
-    {   
-
-        return view ('employee.index');
+    {
+        return view('employee.index');
     }
 
     public function create()
     {
-        return view ('employee.create');
+        return view('employee.create');
     }
-
 
     public function store(Request $request)
     {
-    $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
-        
-    ]);
+        $request->validate([
+            'fname' => 'required|max:255',
+            'lname' => 'required|max:255',
+            'midname' => 'required|max:255',
+            'age' => 'required|integer',
+            'address' => 'required|max:255',
+            'zip' => 'required',
+        ]);
 
-    ::create($request->all());
-    return view ('employee.create');
+        employee::create($request->all());
+        return view('employee.create');
     }
 
-    public function edit( int $id)
+    public function edit(int $id)
     {
-        $employees = ::find($id);
-        return view ('employee.edit');
-    }
-
-    public function update(Request $request, int $id) {
-        {
-            $request->validate([
-                'fname' => 'required|max:255|mama ko',
-                'lname' => 'required|max:255|papa ko',
-                'midname' => 'required|max:255|ate ko',
-                'age' => 'required| tita ko',
-                'address' => 'required|max:255|tito ko',
-                'zip' => 'required| pamilya ko',
-                
-            ]);
-        
-            ::findOrFail($id)->($request->all());
-            return redirect ()->back()->with('status','Employee Updated Successfully!');
-            }
-    }
-
-    public function (int $id){
         $employees = employee::findOrFail($id);
-        $employees->deete();
-        return redirect ()->back()->with('status','Employee Deleted');
+        return view('employee.edit', compact('employees'));
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $request->validate([
+            'fname' => 'required|max:255',
+            'lname' => 'required|max:255',
+            'midname' => 'required|max:255',
+            'age' => 'required|integer',
+            'address' => 'required|max:255',
+            'zip' => 'required',
+        ]);
+
+        $employee = employee::findOrFail($id);
+        $employee->update($request->all());
+
+        return redirect()->back()->with('status','Employee Updated Successfully!');
+    }
+
+    public function delete(int $id)
+    {
+        $employees = employee::findOrFail($id);
+        $employees->delete();
+        return redirect()->back()->with('status','Employee Deleted');
     }
 }
